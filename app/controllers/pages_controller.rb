c83ba4305b5c4cb81e33a25162022de6a20a5ebb
faezrah <faezrah@ideariverrun.com>
2015-06-20 19:41:39 +0800

    move my bookings to index using partial

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
   before_action :require_user, only: [:booking]
 
   def index
+    if current_user
+      @user_bookings = current_user.bookings
+    end
   end
 
   def booking
